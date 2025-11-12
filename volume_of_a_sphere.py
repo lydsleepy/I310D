430dc18ee80d78b3f5333926987b8755ef1d3f7e
lydsleepy <jiahua.mp3@gmail.com>
2025-11-12 10:30:55 -0600

    added volume_of_a_sphere.py

---
+++ volume_of_a_sphere.py
@@ -0,0 +1,9 @@
+
+import math
+
+def calculate_volume_of_a_sphere(radius):
+    volume = (3/4)*(math.pi)*(radius**3)
+    return volume
+
+print(calculate_volume_of_a_sphere(30))
+print(calculate_volume_of_a_sphere(40))
